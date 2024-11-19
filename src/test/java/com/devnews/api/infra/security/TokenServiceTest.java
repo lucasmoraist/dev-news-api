f2351cf5cab283b68fcad405577b93e9cc09c222
lucasmoraist <luksmnt1101@gmail.com>
2024-11-19 13:32:28 -0300

    test: adding test to recover email

--- src/test/java/com/devnews/api/infra/security/TokenServiceTest.java
+++ src/test/java/com/devnews/api/infra/security/TokenServiceTest.java
@@ -1,19 +1,31 @@
 package com.devnews.api.infra.security;
 
+import com.devnews.api.domain.exception.IllegalPermissionException;
 import com.devnews.api.domain.exception.TokenException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class TokenServiceTest {
 
     @InjectMocks
     private TokenService tokenService;
+
+    @Mock
+    private SecurityContext securityContext;
+
+    @Mock
+    private Authentication authentication;
 
     @BeforeEach
     void setUp() {
@@ -77,6 +89,36 @@ class TokenServiceTest {
         });
 
         assertTrue(exception.getMessage().contains("Token inválido"));
+    }
+
+    @Test
+    @DisplayName("Recupera email do usuário logado com sucesso")
+    void case05() {
+        // Arrange
+        String email = "logged@example.com";
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn(email);
+        SecurityContextHolder.setContext(securityContext);
+
+        // Act
+        String loggedEmail = tokenService.recoverLoggedEmail();
+
+        // Assert
+        assertEquals(email, loggedEmail, "O email recuperado deve ser igual ao do usuário logado");
+    }
+
+    @Test
+    @DisplayName("Falha ao recuperar email do usuário logado")
+    void case06() {
+        // Arrange
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("anonymousUser");
+        SecurityContextHolder.setContext(securityContext);
+
+        // Act & Assert
+        IllegalPermissionException exception = assertThrows(IllegalPermissionException.class,
+                () -> tokenService.recoverLoggedEmail());
+        assertEquals("Você não está logado para realizar esta ação.", exception.getMessage());
     }
 
 }
