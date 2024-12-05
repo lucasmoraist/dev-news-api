f0713cd48f5dbf9ef165b8ccad682d68e5395b93
lucasmoraist <luksmnt1101@gmail.com>
2024-12-05 15:38:07 -0300

    feat: adding roles to CORS

--- src/main/java/com/devnews/api/infra/cors/CorsConfig.java
+++ src/main/java/com/devnews/api/infra/cors/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "DELETE", "PUT")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PATCH")
                         .allowedHeaders("*");
             }};
     }
